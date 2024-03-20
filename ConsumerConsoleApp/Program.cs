7e397ac380e8c5037958620bae407f1dbd9dadc0
learndotnet78 <learndotnet78@hotmail.com>
2024-03-20 14:26:00 -0400

    Initial commit

---
+++ ConsumerConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consume-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("weather-topic");
+
+CancellationToken token = new();
+
+
+try
+{
+    string? state;
+    while (true)
+    {
+        var response = consumer.Consume(token);
+        if (response != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            if (weather != null)
+            {
+                Console.WriteLine($"State - {weather.State} and Temperature - {weather.Temperature}F");
+            }
+        }
+    }
+}
+catch (ProduceException<Null,string> ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Weather(string State, int Temperature);
---
+++ ProduceConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    string? state;
+    while ((state = Console.ReadLine()) != null)
+    {
+        var response = producer.ProduceAsync("weather-topic",
+    new Message<Null, string> { Value = JsonConvert.SerializeObject(new Weather(state, 70)) });
+    }
+}
+catch (ProduceException<Null, string> ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Weather(string State, int Temperature);
